138729068821848b15397ada2c18614061f494ac
Alfi74 <catwyzle@ymail.com>
2023-08-31 17:21:05 +0200

    p1

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+fetch("data.json")
+  .then((antwortvondatajson) => {
+    return antwortvondatajson.json();
+  })
+  .then((alleusersdatenausdatajson) => {
+    const container = document.getElementById("users");
+    alleusersdatenausdatajson.forEach((ergeabnisderforeache) => {
+      const tmpl = document
+        .getElementById("userCardTemplate")
+        .content.cloneNode(true);
+      tmpl.querySelector(".fullname").innerText = ergeabnisderforeache.name;
+      tmpl.querySelector(".title").innerHTML = ergeabnisderforeache.title;
+      tmpl.querySelector(".profession").innerText = ergeabnisderforeache.profession;
+      tmpl.querySelector(".description").innerText = ergeabnisderforeache.description;
+      tmpl.querySelector("img").setAttribute("src", ergeabnisderforeache.avatar);
+      container.appendChild(tmpl);
+    });
+  });
